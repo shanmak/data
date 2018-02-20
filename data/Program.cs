65ab05c6349a82b7a531b178e68d4f4927da8e27
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-02-20 22:43:28 +0800

    hi

--- data/Program.cs
+++ data/Program.cs
@@ -10,6 +10,17 @@ namespace data
     {
         static void Main(string[] args)
         {
+            Person p = new Person("","","","");
+            Gender m1 = new Gender("mani", "male", "india", "83134567");
+            Gender m2;
+            m2 = new Gender("thama", "female", "singapore", "6543289");
+           Console.WriteLine( m1.DIsplay());
+            Console.WriteLine();
+            Console.WriteLine(m2.DIsplay());
+            Console.WriteLine();
+            m2.Name = "thaya";
+            Console.WriteLine(m2.DIsplay());
+            
         }
     }
 }
---
+++ data/person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data
+{
+   public class Person
+    {
+        string name;
+        string addres;
+        string gender;
+        string phone;
+
+        public Person(string a, string b, string c, string d)
+        {
+            name = a;
+            addres = b;
+            gender = c;
+            phone = d;
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string Address
+        {
+            set
+            {
+                addres = value;
+            }
+            get
+            {
+                return addres;
+            }
+        }
+        public string Gender
+        {
+            set
+            {
+                gender = value;
+            }
+            get
+            {
+                return gender;
+            }
+        }
+        public string Phone
+        {
+            set
+            {
+                phone = value;
+            }
+            get
+            {
+                return phone;
+            }
+        }
+        public string Display()
+        {
+            string s = string.Format("name={0}\ngender={1}\naddress={2}\nphone={3}", Name, Gender, Address, Phone);
+            return s;
+        }
+    }
+
+    class Gender:Person
+    {
+        public Gender(string a, string b, string c, string d) : base(a, b, c, d)
+        {
+        }
+        public string DIsplay()
+        {
+            return base.Display();
+        }
+    }
+}
